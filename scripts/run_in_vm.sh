0125c4e072462c74f0df504532a66ac8a3272479
Jakub Latusek <j.latusek@samsung.com>
2024-03-27 13:40:32 +0100

    Add run in qemu vm script

---
+++ scripts/run_in_vm.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2024 Project CHIP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This script executes the command given as an argument after
+# activating the given python virtual environment
+set -e
+
+SCRIPT_PATH=$(dirname "$(dirname "$(realpath "$0")")")
+
+echo "$@" >"$SCRIPT_PATH/runner.sh"
+chmod +x "$SCRIPT_PATH/runner.sh"
+
+"$SCRIPT_PATH/integrations/docker/images/stage-2/chip-build-linux-qemu/run-img.sh"
+
+if [ -f "$SCRIPT_PATH/runner_status" ]; then
+	exit "$(cat $SCRIPT_PATH/runner_status)"
+else
+	exit 1
+fi
