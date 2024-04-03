d626e6ac35c0ba06d7899796f9f8eeeb9430a08f
Jakub Latusek <j.latusek@samsung.com>
2024-04-03 11:22:18 +0200

    Use priv container

--- integrations/docker/images/stage-2/chip-build-linux-qemu/run-img.sh
+++ integrations/docker/images/stage-2/chip-build-linux-qemu/run-img.sh
@@ -2,24 +2,28 @@
 
 KERNEL="/opt/ubuntu-qemu/bzImage"
 IMG="/opt/ubuntu-qemu/ubuntu-20.04.img"
-ADDITIONAL_ARGS=""
+ADDITIONAL_ARGS=()
 PROJECT_PATH="$(realpath "$(dirname "$0")/../../../../..")"
 
-if kvm-ok; then
-	ADDITIONAL_ARGS="-enable-kvm -cpu host"
-else
-	echo "KVM is not available, running without it."
-fi
+# if kvm-ok; then
+#     ADDITIONAL_ARGS+=("-enable-kvm" "-cpu" "host")
+# else
+#     echo "KVM is not available, running without it."
+# fi
 
 /usr/bin/qemu-system-x86_64 \
-	-machine ubuntu \
-	-smp 6 \
-	-m 1024 \
-	-nographic \
-	-device virtio-blk-pci,drive=virtio-blk1 \
-	-drive file=${IMG},id=virtio-blk1,if=none,format=qcow2,readonly=off \
-	-kernel ${KERNEL} \
-	-append 'console=ttyS0 mac80211_hwsim.radios=2 root=/dev/vda3' \
-	-netdev user,id=network0,hostfwd=tcp::2222-:22 \
-	-device e1000,netdev=network0,mac=52:54:00:12:34:56 \
-	-virtfs "local,path=$PROJECT_PATH,mount_tag=host0,security_model=passthrough,id=host0" $ADDITIONAL_ARGS
+    -machine ubuntu \
+    -smp 2 \
+    -m 1024 \
+    -nographic \
+    -monitor null \
+    -serial stdio \
+    -display none \
+    -device virtio-blk-pci,drive=virtio-blk1 \
+    -drive file="$IMG",id=virtio-blk1,if=none,format=qcow2,readonly=off \
+    -kernel "$KERNEL" \
+    -append 'console=ttyS0 mac80211_hwsim.radios=2 root=/dev/vda3' \
+    -netdev user,id=network0,hostfwd=tcp::2222-:22 \
+    -device e1000,netdev=network0,mac=52:54:00:12:34:56 \
+    -virtfs "local,path=$PROJECT_PATH,mount_tag=host0,security_model=passthrough,id=host0" \
+    "${ADDITIONAL_ARGS[@]}"
