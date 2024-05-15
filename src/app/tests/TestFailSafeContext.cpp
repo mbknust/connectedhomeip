50ad862d760dc96b1734e12fb0a586e29ac83fa8
Arkadiusz Bokowy <a.bokowy@samsung.com>
2024-05-15 16:40:19 +0200

    TestFailSafeContext

--- src/app/tests/TestFailSafeContext.cpp
+++ src/app/tests/TestFailSafeContext.cpp
@@ -28,11 +28,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <gtest/gtest.h>
+
 #include <app/FailSafeContext.h>
 #include <lib/support/CHIPMem.h>
 #include <lib/support/CodeUtils.h>
-#include <lib/support/UnitTestRegistration.h>
-#include <nlunit-test.h>
 #include <platform/CHIPDeviceLayer.h>
 
 using namespace chip;
@@ -44,100 +44,58 @@ namespace {
 constexpr FabricIndex kTestAccessingFabricIndex1 = 1;
 constexpr FabricIndex kTestAccessingFabricIndex2 = 2;
 
-// =================================
-//      Unit tests
-// =================================
-
-static void TestPlatformMgr_Init(nlTestSuite * inSuite, void * inContext)
+class TestFailSafeContext : public ::testing::Test
 {
-    CHIP_ERROR err = PlatformMgr().InitChipStack();
-    NL_TEST_ASSERT(inSuite, err == CHIP_NO_ERROR);
-}
-
-static void TestFailSafeContext_ArmFailSafe(nlTestSuite * inSuite, void * inContext)
-{
-    CHIP_ERROR err = CHIP_NO_ERROR;
-
-    chip::app::FailSafeContext failSafeContext;
-
-    err = failSafeContext.ArmFailSafe(kTestAccessingFabricIndex1, System::Clock::Seconds16(1));
-    NL_TEST_ASSERT(inSuite, err == CHIP_NO_ERROR);
-    NL_TEST_ASSERT(inSuite, failSafeContext.IsFailSafeArmed() == true);
-    NL_TEST_ASSERT(inSuite, failSafeContext.GetFabricIndex() == kTestAccessingFabricIndex1);
-    NL_TEST_ASSERT(inSuite, failSafeContext.IsFailSafeArmed(kTestAccessingFabricIndex1) == true);
-    NL_TEST_ASSERT(inSuite, failSafeContext.IsFailSafeArmed(kTestAccessingFabricIndex2) == false);
-
-    failSafeContext.DisarmFailSafe();
-    NL_TEST_ASSERT(inSuite, failSafeContext.IsFailSafeArmed() == false);
-}
-
-static void TestFailSafeContext_NocCommandInvoked(nlTestSuite * inSuite, void * inContext)
-{
-    CHIP_ERROR err = CHIP_NO_ERROR;
-
-    chip::app::FailSafeContext failSafeContext;
-
-    err = failSafeContext.ArmFailSafe(kTestAccessingFabricIndex1, System::Clock::Seconds16(1));
-    NL_TEST_ASSERT(inSuite, err == CHIP_NO_ERROR);
-    NL_TEST_ASSERT(inSuite, failSafeContext.GetFabricIndex() == kTestAccessingFabricIndex1);
-
-    failSafeContext.SetAddNocCommandInvoked(kTestAccessingFabricIndex2);
-    NL_TEST_ASSERT(inSuite, failSafeContext.NocCommandHasBeenInvoked() == true);
-    NL_TEST_ASSERT(inSuite, failSafeContext.AddNocCommandHasBeenInvoked() == true);
-    NL_TEST_ASSERT(inSuite, failSafeContext.GetFabricIndex() == kTestAccessingFabricIndex2);
-
-    failSafeContext.SetUpdateNocCommandInvoked();
-    NL_TEST_ASSERT(inSuite, failSafeContext.NocCommandHasBeenInvoked() == true);
-    NL_TEST_ASSERT(inSuite, failSafeContext.UpdateNocCommandHasBeenInvoked() == true);
-
-    failSafeContext.DisarmFailSafe();
-}
-
-/**
- *   Test Suite. It lists all the test functions.
- */
-static const nlTest sTests[] = {
-
-    NL_TEST_DEF("Test PlatformMgr::Init", TestPlatformMgr_Init),
-    NL_TEST_DEF("Test FailSafeContext::ArmFailSafe", TestFailSafeContext_ArmFailSafe),
-    NL_TEST_DEF("Test FailSafeContext::NocCommandInvoked", TestFailSafeContext_NocCommandInvoked),
-
-    NL_TEST_SENTINEL()
+public:
+    static void SetUpTestSuite()
+    {
+        ASSERT_EQ(chip::Platform::MemoryInit(), CHIP_NO_ERROR);
+        ASSERT_EQ(PlatformMgr().InitChipStack(), CHIP_NO_ERROR);
+    }
+    static void TearDownTestSuite()
+    {
+        PlatformMgr().Shutdown();
+        chip::Platform::MemoryShutdown();
+    }
 };
 
-/**
- *  Set up the test suite.
- */
-int TestFailSafeContext_Setup(void * inContext)
+TEST_F(TestFailSafeContext, ArmFailSafe)
 {
-    CHIP_ERROR error = chip::Platform::MemoryInit();
-    if (error != CHIP_NO_ERROR)
-        return FAILURE;
-    return SUCCESS;
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    chip::app::FailSafeContext failSafeContext;
+
+    err = failSafeContext.ArmFailSafe(kTestAccessingFabricIndex1, System::Clock::Seconds16(1));
+    EXPECT_EQ(err, CHIP_NO_ERROR);
+    EXPECT_EQ(failSafeContext.IsFailSafeArmed(), true);
+    EXPECT_EQ(failSafeContext.GetFabricIndex(), kTestAccessingFabricIndex1);
+    EXPECT_EQ(failSafeContext.IsFailSafeArmed(kTestAccessingFabricIndex1), true);
+    EXPECT_EQ(failSafeContext.IsFailSafeArmed(kTestAccessingFabricIndex2), false);
+
+    failSafeContext.DisarmFailSafe();
+    EXPECT_EQ(failSafeContext.IsFailSafeArmed(), false);
 }
 
-/**
- *  Tear down the test suite.
- */
-int TestFailSafeContext_Teardown(void * inContext)
+TEST_F(TestFailSafeContext, NocCommandInvoked)
 {
-    PlatformMgr().Shutdown();
-    chip::Platform::MemoryShutdown();
-    return SUCCESS;
+    CHIP_ERROR err = CHIP_NO_ERROR;
+
+    chip::app::FailSafeContext failSafeContext;
+
+    err = failSafeContext.ArmFailSafe(kTestAccessingFabricIndex1, System::Clock::Seconds16(1));
+    EXPECT_EQ(err, CHIP_NO_ERROR);
+    EXPECT_EQ(failSafeContext.GetFabricIndex(), kTestAccessingFabricIndex1);
+
+    failSafeContext.SetAddNocCommandInvoked(kTestAccessingFabricIndex2);
+    EXPECT_EQ(failSafeContext.NocCommandHasBeenInvoked(), true);
+    EXPECT_EQ(failSafeContext.AddNocCommandHasBeenInvoked(), true);
+    EXPECT_EQ(failSafeContext.GetFabricIndex(), kTestAccessingFabricIndex2);
+
+    failSafeContext.SetUpdateNocCommandInvoked();
+    EXPECT_EQ(failSafeContext.NocCommandHasBeenInvoked(), true);
+    EXPECT_EQ(failSafeContext.UpdateNocCommandHasBeenInvoked(), true);
+
+    failSafeContext.DisarmFailSafe();
 }
 
 } // namespace
-
-/**
- *  Main
- */
-int TestFailSafeContext()
-{
-    nlTestSuite theSuite = { "FailSafeContext tests", &sTests[0], TestFailSafeContext_Setup, TestFailSafeContext_Teardown };
-
-    // Run test suite against one context.
-    nlTestRunner(&theSuite, nullptr);
-    return nlTestRunnerStats(&theSuite);
-}
-
-CHIP_REGISTER_TEST_SUITE(TestFailSafeContext)
